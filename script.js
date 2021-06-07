bf517aff29b754b71d316fdabf6084c4b0d4f9c8
Andrew Knight <knightandrew21@icloud.com>
2021-06-07 10:10:34 -0700

    refactored code adding obj class for gameBoard and snake

--- script.js
+++ script.js
@@ -1,80 +1,95 @@
 const canvas = document.getElementById('game-canvas');
 const canvasContext = canvas.getContext('2d');
+let gameOn = true;
 
 /* ----- Gameboard ----- */
-function drawGameBoard(color1, color2, color3, boardWidth, boardHeight) {
-  canvasContext.fillStyle = color1;
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-  let alternateRowColorPattern = true;
-  for (let y = 0; y < canvas.height; y += boardHeight) {
-    if (alternateRowColorPattern) {
-      for (let x = 0; x < canvas.width; x += 2 * boardWidth) {
-        canvasContext.fillStyle = color2;
-        canvasContext.fillRect(x, y, boardWidth, boardHeight);
+class GameBoard {
+  constructor(color1, color2, color3, boardWidth, boardHeight) {
+    this.color1 = color1;
+    this.color2 = color2;
+    this.color3 = color3;
+    this.boardWidth = boardWidth;
+    this.boardHeight = boardHeight;
+
+    this.checkerBoardPattern = function() {
+      canvasContext.fillStyle = color1;
+      canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+      let alternateRowColorPattern = true;
+      for (let y = 0; y < canvas.height; y += boardHeight) {
+        if (alternateRowColorPattern) {
+          for (let x = 0; x < canvas.width; x += 2 * boardWidth) {
+            canvasContext.fillStyle = color2;
+            canvasContext.fillRect(x, y, boardWidth, boardHeight);
+          }
+          alternateRowColorPattern = false;
+        } else {
+          for (let x = boardWidth; x < canvas.width; x += 2 * boardWidth) {
+            canvasContext.fillStyle = color2;
+            canvasContext.fillRect(x, y, boardWidth, boardHeight);
+          }
+          alternateRowColorPattern = true;
+        }
       }
-      alternateRowColorPattern = false;
-    } else {
-      for (let x = boardWidth; x < canvas.width; x += 2 * boardWidth) {
-        canvasContext.fillStyle = color2;
-        canvasContext.fillRect(x, y, boardWidth, boardHeight);
-      }
-      alternateRowColorPattern = true;
-    }
+    };
+    
+    this.gameBoardBorder = function() {
+      canvasContext.strokeStyle = color3;
+      canvasContext.strokeRect(0, 0, canvas.width, canvas.height);
+    };
   }
-  // gameboard border
-  canvasContext.strokeStyle = color3;
-  canvasContext.strokeRect(0, 0, canvas.width, canvas.height);
 }
+const gameBoard = new GameBoard('#8ed728', '#7128d7', '#000', 20, 20);
 
 /* ----- Snake ----- */
-const snakeXPosition = canvas.width / 4;
-const snakeYPosition = canvas.height / 2;
-const snakeWidth = 20;
-const snakeHeight = 20;
-let snakeDirection = '';
+let snakeDirection = null;
+let snakeBody = [];
 
-// generating snake
-let snake = [];
-for (let currentLink = 0; currentLink < 4; currentLink++) {
-  snake.push({x: snakeXPosition - (currentLink * snakeWidth), y: snakeYPosition});
-}
+class Snake {
+  constructor() {
+    const snakeXPosition = canvas.width / 4;
+    const snakeYPosition = canvas.height / 2;
+    const snakeWidth = 20;
+    const snakeHeight = 20;
 
-function drawSnake() {
-  canvasContext.strokeStyle = 'red';
-  canvasContext.fillStyle = '#fbb104';
-  snake.forEach((snakeLink) => {
-    canvasContext.fillRect(snakeLink.x, snakeLink.y, snakeWidth, snakeHeight);
-    canvasContext.strokeRect(snakeLink.x, snakeLink.y, snakeWidth, snakeHeight);
-  });
-}
+    this.generateSnakeBody = function() {
+      for(let currentLink = 0; currentLink < 4; currentLink++) {
+        snakeBody.push({x: snakeXPosition - (currentLink * snakeWidth), y: snakeYPosition});
+      }
+    };
 
-function moveSnake() {
-  const snakeSpeed = 20;
-  const head = {x: snake[0].x, y: snake[0].y};
-  switch (snakeDirection) {
-    case 'right':
-      head.x += snakeSpeed;
-      break;
-    case 'left':
-      head.x -= snakeSpeed;
-      break;
-    case 'up':
-      head.y -= snakeSpeed;
-      break;
-    case 'down':
-      head.y += snakeSpeed;
-      break;
+    this.drawSnake = function() {
+      canvasContext.strokeStyle = 'red';
+      canvasContext.fillStyle = '#fbb104';
+      snakeBody.forEach((snakeBodyLink) => {
+        canvasContext.fillRect(snakeBodyLink.x, snakeBodyLink.y, snakeWidth, snakeHeight);
+        canvasContext.strokeRect(snakeBodyLink.x, snakeBodyLink.y, snakeWidth, snakeHeight);
+      });
+    };
+
+    this.moveSnake = function() {
+      const snakeSpeed = 20;
+      const head = {x: snakeBody[0].x, y: snakeBody[0].y};
+      switch (snakeDirection) {
+        case 'right':
+          head.x += snakeSpeed;
+          break;
+        case 'left':
+          head.x -= snakeSpeed;
+          break;
+        case 'up':
+          head.y -= snakeSpeed;
+          break;
+        case 'down':
+          head.y += snakeSpeed;
+          break;
+      }
+      snakeBody.unshift(head);
+      snakeBody.pop();
+    };
   }
-  snake.unshift(head);
-  snake.pop();
 }
-
-function drawFrame() {
-  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-  drawGameBoard('#8ed728', '#7128d7', '#000', 20, 20);
-  moveSnake();
-  drawSnake();
-}
+const snake = new Snake();
+snake.generateSnakeBody();
 
 /* ----- Game Controls ----- */
 function keyEventHandler(event) {
@@ -104,7 +119,7 @@ function keyEventHandler(event) {
   }
 };
 
-/* ----- Pause/Play ----- */
+// /* ----- Pause/Play ----- */
 class IntervalTimer {
   constructor(callback, delay) {
     let timerId;
@@ -129,22 +144,23 @@ class IntervalTimer {
     this.resume();
   }
 }
-const animation = new IntervalTimer(drawFrame, 400);
 
-/* ----- Animation ----- */
+/* ----- game at each frame ----- */
 function drawFrame() {
-  // canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-  drawGameBoard('#8ed728', '#7128d7', '#000', 20, 20);
-  drawSnake();
-  moveSnake();
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+  gameBoard.checkerBoardPattern();
+  gameBoard.gameBoardBorder();
+
+  snake.drawSnake();
+  snake.moveSnake();
 }
 
-let gameOn = true;
-window.addEventListener('load', function() {
-  drawGameBoard('#8ed728', '#7128d7', '#000', 20, 20);
-  drawSnake(); 
+/* ----- Animation ----- */
+const animation = new IntervalTimer(drawFrame, 400);
 
-  document.addEventListener('keydown', keyEventHandler);
+window.addEventListener('load', function() {
+  drawFrame(gameBoard, snake);
+  // document.addEventListener('keydown', keyEventHandler);
 });
 
 
@@ -154,4 +170,92 @@ window.addEventListener('load', function() {
 
 
 
+
+
+
+
+
+/* ----- Gameboard ----- */
+// function drawGameBoard(color1, color2, color3, boardWidth, boardHeight) {
+//   canvasContext.fillStyle = color1;
+//   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+//   let alternateRowColorPattern = true;
+//   for (let y = 0; y < canvas.height; y += boardHeight) {
+//     if (alternateRowColorPattern) {
+//       for (let x = 0; x < canvas.width; x += 2 * boardWidth) {
+//         canvasContext.fillStyle = color2;
+//         canvasContext.fillRect(x, y, boardWidth, boardHeight);
+//       }
+//       alternateRowColorPattern = false;
+//     } else {
+//       for (let x = boardWidth; x < canvas.width; x += 2 * boardWidth) {
+//         canvasContext.fillStyle = color2;
+//         canvasContext.fillRect(x, y, boardWidth, boardHeight);
+//       }
+//       alternateRowColorPattern = true;
+//     }
+//   }
+//   canvasContext.strokeStyle = color3;
+//   canvasContext.strokeRect(0, 0, canvas.width, canvas.height);
+// }
+
+/* ----- Snake ----- */
+// const snakeXPosition = canvas.width / 4;
+// const snakeYPosition = canvas.height / 2;
+// const snakeWidth = 20;
+// const snakeHeight = 20;
+// let snakeDirection = '';
+
+// let snake = [];
+// for (let currentLink = 0; currentLink < 4; currentLink++) {
+//   snake.push({x: snakeXPosition - (currentLink * snakeWidth), y: snakeYPosition});
+// }
+
+// function drawSnake() {
+//   canvasContext.strokeStyle = 'red';
+//   canvasContext.fillStyle = '#fbb104';
+//   snake.forEach((snakeLink) => {
+//     canvasContext.fillRect(snakeLink.x, snakeLink.y, snakeWidth, snakeHeight);
+//     canvasContext.strokeRect(snakeLink.x, snakeLink.y, snakeWidth, snakeHeight);
+//   });
+// }
+
+// function moveSnake() {
+//   const snakeSpeed = 20;
+//   const head = {x: snake[0].x, y: snake[0].y};
+//   switch (snakeDirection) {
+//     case 'right':
+//       head.x += snakeSpeed;
+//       break;
+//     case 'left':
+//       head.x -= snakeSpeed;
+//       break;
+//     case 'up':
+//       head.y -= snakeSpeed;
+//       break;
+//     case 'down':
+//       head.y += snakeSpeed;
+//       break;
+//   }
+//   snake.unshift(head);
+//   snake.pop();
+// }
+
+// function drawFrame() {
+//   canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+//   gameBoard('#8ed728', '#7128d7', '#000', 20, 20);
+
+//   moveSnake();
+//   drawSnake();
+// }
+
+
+
+/* ----- Animation ----- */
+// function drawFrame() {
+//   // canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+//   drawGameBoard('#8ed728', '#7128d7', '#000', 20, 20);
+//   drawSnake();
+//   moveSnake();
+// }
 
